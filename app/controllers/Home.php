d1efaed47988dfa03dcbb79d1f5819129dea80e8
Michael <Michael@152.105.90.61>
2014-11-15 18:38:27 +0000

    Updated Skeleton code to complete the routing process of the MVC structure.

---
+++ app/controllers/Home.php
@@ -0,0 +1,16 @@
+<?php
+/*
+* Final Year Project - Assessment Database
+* @author Michael Leah
+*/
+
+class Home extends Controller
+{
+    public function index($profileID = '')
+    {
+       echo 'Test Method: ' . $profileID;
+    }
+    
+    
+    
+}
--- app/core/App.php
+++ app/core/App.php
@@ -6,9 +6,45 @@
 
 class App 
 {
+    // Default Controller, Method and Parameters.
+    protected $controller = 'home';
+    protected $method = 'index';
+    protected $params = [];
+    
     public function __construct()
     {
-        echo 'Test!';
+        $url = $this->parseUrl();
+        
+               
+        // Check Controller exists
+        if (file_exists('../app/controllers/' . $url[0] . '.php')) {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+        
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+        
+        // Check Method exists
+        if (isset($url[1])){
+            if(method_exists($this->controller, $url[1])){
+               $this->method = $url[1];
+               unset($url[1]);               
+            }
+        }
+       
+        //  Prevents Array error if no values are specified.
+        $this->params = $url ? array_values($url) : [];
+        call_user_func_array([$this->controller, $this->method], $this->params);
+        
+    }
+    
+    // Sanitising the URL for MVC routing
+    protected function parseUrl()
+    {
+        if(isset($_GET['url'])) {
+            return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+        }
     }
 }   
 
